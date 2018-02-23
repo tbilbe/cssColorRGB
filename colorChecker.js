ff3ba7178728abc862a62ee58a88b7e77d667a99
Tom Bilbe <tbilbe@gmail.com>
2018-02-23 11:58:48 +0000

    commit test using vscode gui

--- colorChecker.js
+++ colorChecker.js
@@ -1,1 +1,6 @@
 console.log('test')
+console.log('test from work computer')
+
+// testing setting up a git clone https://github.com/tbilbe/cssColorRGB.git
+// then when i am home comp i should use git fetch origin
+// i think!
